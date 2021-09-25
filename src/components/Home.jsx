408791e0b1e7e174476c33501295f54201f7e992
Chris Burton <chris@fullstackcaveman.com>
2021-09-25 12:25:21 -0600

    initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+	return (
+		<div>
+			<h2>Home Works</h2>
+		</div>
+	);
+};
+
+export default Home;
---
+++ src/components/elements/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+	return (
+		<div>
+			<h2>Footer Works</h2>
+		</div>
+	);
+};
+
+export default Footer;
